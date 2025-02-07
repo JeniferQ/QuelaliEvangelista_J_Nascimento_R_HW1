bef5b2335696bd9f302a75a695c4b392297e3662
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2025-02-06 23:44:30 -0500

    Add dynamic movie information
    In this commit, the dynamic movie section was created to fetch movies from the Star Wars API.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (() => {
     const charList = document.querySelector('#char-list'),
+    charBanner = document.querySelector('.char-banner'),
+    charDetails = document.querySelector('#char-details'),
+    movieTemplate = document.querySelector('.movie-template'),
+    movieList= document.querySelector('#movie-list'),
     baseURL = 'https://swapi.dev/api/';
 
 function getCharacters() {
@@ -40,12 +44,53 @@ function getCharacters() {
         })
     })
     .catch(function(error) {
-        charList.innerHTML = "<p>This character couldn't be loaded.</p>"
+        charList.innerHTML = "<p>This character couldn't be loaded.</p>";
         console.log(error);
     })
 }
 
+
 function getInfo(e) {
+    const charName = document.querySelector('.char-name');
+    const selected = e.currentTarget.dataset.name;
+
+    let nameNoSpaces = selected.replace(/\s+/g, '_');
+    charName.textContent = selected;
+
+    charBanner.style.backgroundImage = `url(../images/${nameNoSpaces}_banner.jpg)`;
+
+    const movies = e.currentTarget.dataset.films.split(',');
+    charDetails.style.display = 'grid';
+
+    const movieLoader = document.querySelector('#loader2');
+    movieList.innerHTML = '';
+
+    movies.forEach(movie => {
+        fetch(movie)
+        .then(response => response.json())
+        .then(function(response) {
+            const clone = movieTemplate.content.cloneNode(true);
+            movieLoader.classList.add("hidden");
+            
+            const movieTitle = response.title;
+            let bannerNoSpaces = movieTitle.replace(/\s+/g, '_');
+
+            const moviePoster = clone.querySelector('.movie-img');
+            moviePoster.src = `images/${bannerNoSpaces}.jpg`;
+
+            const movieName = clone.querySelector('.movie-name');
+            movieName.textContent = response.title;
+    
+            const movieCrawl = clone.querySelector('.movie-release');
+            movieCrawl.textContent = response.opening_crawl;
+    
+            movieList.appendChild(clone);
+        })
+        .catch(function(error) {
+            movieList.innerHTML = "<p>This character does not participate in a movie.</p>";
+            console.log(error);
+        });
+    });
 }
 
 getCharacters();
